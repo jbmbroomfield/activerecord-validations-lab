578fb5117ed7f316513fc2c4135c4c25042957fe
jim <jbmbroomfield@gmail.com>
2021-08-30 14:33:53 +1000

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate  :title_must_be_clickbaity
+
+    def title_must_be_clickbaity
+        ["Won't Believe", "Secret", "Top [number]", "Guess"].each do |buzzword|
+            return if title && title.include?(buzzword)
+        end
+        errors.add(:title, 'must be clickbaity')
+    end
+
 end
